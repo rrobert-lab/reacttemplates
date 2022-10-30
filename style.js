52f164828a8490103418c98b4a074c33b2f5d1db
rrobert-lab <ravinrobert@gmail.com>
2022-10-30 00:01:29 -0400

    c

--- style.js
+++ style.js
@@ -1,11 +1,16 @@
 import React from "react"
 import boxes from "./boxes"
 
-export default function App() {
+export default function App(props) {
     const [squares, setSquares] = React.useState(boxes)
+    props.darkMode
+    
+    // Challenge: use a ternary to determine the backgroundColor.
+    // If darkMode is true, set it to "#222222"
+    // If darkMode is false, set it to "#cccccc"
     
     const styles = {
-        backgroundColor: "black"
+        backgroundColor:props.darkMode?"#222222":"#cccccc" // You code here
     }
     
     const squareElements = squares.map(square => (
