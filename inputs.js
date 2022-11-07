0dee0068fdda3f24292e45f06b2b75daaa564bdc
rrobert-lab <ravinrobert@gmail.com>
2022-11-07 02:37:04 -0500

    c

--- inputs.js
+++ inputs.js
@@ -8,7 +8,8 @@ export default function Form() {
             email: "", 
             comments: "", 
             isFriendly: true,
-            employment: ""
+            employment: "",
+            favColor: ""
         }
     )
     
@@ -22,8 +23,14 @@ export default function Form() {
         })
     }
     
+    function handleSubmit(event) {
+        event.preventDefault()
+        // submitToApi(formData)
+        console.log(formData)
+    }
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
                 type="text"
                 placeholder="First Name"
@@ -64,11 +71,13 @@ export default function Form() {
             
             <fieldset>
                 <legend>Current employment status</legend>
-                
                 <input 
                     type="radio"
                     id="unemployed"
                     name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
                 />
                 <label htmlFor="unemployed">Unemployed</label>
                 <br />
@@ -77,6 +86,9 @@ export default function Form() {
                     type="radio"
                     id="part-time"
                     name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
                 />
                 <label htmlFor="part-time">Part-time</label>
                 <br />
@@ -85,11 +97,34 @@ export default function Form() {
                     type="radio"
                     id="full-time"
                     name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
                 />
                 <label htmlFor="full-time">Full-time</label>
                 <br />
-                
             </fieldset>
+            <br />
+            
+            <label htmlFor="favColor">What is your favorite color?</label>
+            <br />
+            <select 
+                id="favColor" 
+                value={formData.favColor}
+                onChange={handleChange}
+                name="favColor"
+            >
+                <option value="red">Red</option>
+                <option value="orange">Orange</option>
+                <option value="yellow">Yellow</option>
+                <option value="green">Green</option>
+                <option value="blue">Blue</option>
+                <option value="indigo">Indigo</option>
+                <option value="violet">Violet</option>
+            </select>
+            <br />
+            <br />
+            <button>Submit</button>
         </form>
     )
 }
